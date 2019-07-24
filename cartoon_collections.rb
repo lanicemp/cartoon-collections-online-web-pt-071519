4864443e42a8e11f7975d8eb0d37fed298dcd05c
LaNice Powell <lanicemp@aol.com>
2019-07-24 17:16:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def roll_call_dwarves(dwarves)
 end 
 
 def summon_captain_planet(veggies)
-  veggies == []
-  veggies.each  {|value| puts "#{value}"}
+  veggies.capitalize
+  #veggies.each  {|value| puts "#{value}"}
 
   
   binding.pry
