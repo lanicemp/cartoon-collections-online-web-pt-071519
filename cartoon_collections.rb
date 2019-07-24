54e04b64f6e693c8fd0f3206eb2f151eb9f1b360
LaNice Powell <lanicemp@aol.com>
2019-07-24 17:40:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 
 def summon_captain_planet(veggies)
   veggies.map  {|value|  value.capitalize + "!" }
-  binding.pry 
+ # binding.pry 
 end
 
 
@@ -18,7 +18,8 @@ def long_planeteer_calls(elements)
 end
 end 
 
-def find_the_cheese# code an argument here
+def find_the_cheese (dif_cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  binding.pry
 end
